a48c9dabd289a40137e35ec7d0d8597117b75615
Yusuf NACAR <yusuf.nacar@gtech.com.tr>
2022-07-19 12:09:22 +0300

    coordinator fixed

---
+++ R-MLM/Core/Coordinator/Coordinator.swift
@@ -0,0 +1,10 @@
+
+
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+
+    func start()
+}
